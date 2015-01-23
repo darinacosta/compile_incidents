1fbf2f39ac44fdf1fbf14cc84f576574dc69875f
unknown <djacosta@ITI-1109L.cityofno.com>
2015-01-23 10:38:39 -0600

    incident tracker

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ var http = require("http"),
     incidents = require('./incidents.json'),
     parishes = require('./assets/layers/parishesMerged.json'),
 
-pointInPoly = function(point){
+objectTracker = function(startingCount){
+  this.startingCount = startingCount;
+  this.objectsChecked = 0;
+  this.objectsWritten= 0;
+  this.newRecords = [];
+  this.newRecordDates = [];
+  this.endingCount = function(){
+    return this.startingCount + this.objectsWritten
+  };
+  this.addNewRecord = function(id){
+    this.newRecords.push(id);
+  };
+  this.addNewRecordDate = function(date){
+    this.newRecordDates.push(date);
+  };
+  this.incChecked = function(){
+    this.objectsChecked ++;  
+  };
+  this.incWritten = function(){
+    this.objectsWritten ++;
+  };
+},
+
+incidentTracker = new objectTracker(incidents.length),
+
+pointInTargetArea = function(point){
   var result = gju.pointInPolygon({"type":"Point","coordinates":point}, parishes)
   return result;
 },
@@ -31,28 +56,39 @@ writeToFile = function(content){
     if(e) {
       console.log(e);
     } else {
-        console.log("The file was saved.");
+        console.log("Incidents saved succesfully.");
     }
   })
 },
 
-jsonRecorded = function(entry){
+generateIncidentID = function(guid){
+  var splitArray = guid.split('/');
+  return splitArray[splitArray.length - 1];
+},
+
+recordAlreadyExists = function(record){
   for (var i = 0; i < incidents.length; i++){
-    if (incidents[i]['guid'][0] === entry['guid'][0]){
-      console.log(incidents[i]['guid']  + ' is already recorded')
+    if (incidents[i]['guid'][0] === record['guid'][0]){
       return true;
     }
   }
   return false;
 },
 
-updateIncidents = function(targetData){
-  for (var i = 0; i < targetData.length; i++){
-    if (jsonRecorded(targetData[i]) === false){
-      targetData[i]['georss:point'] = parseLatLong(targetData[i]['georss:point'][0]);
-      if (pointInPoly(targetData[i]['georss:point']) === true){
-        incidents.push(targetData[i]);
-        console.log(targetData[i]['guid'] + ' was recorded');
+formatInputData = function(inputData){
+  inputData['coordinates'] = parseLatLong(inputData['georss:point'][0]);
+  inputData['id'] = generateIncidentID(inputData['guid'][0]);
+},
+
+updateIncidents = function(inputData){
+  for (var i = 0; i < inputData.length; i++){
+    if (recordAlreadyExists(inputData[i]) === false){
+      formatInputData(inputData[i]);
+      if (pointInTargetArea(inputData[i]['coordinates']) === true){
+        incidents.push(inputData[i]);
+        incidentTracker.incWritten();
+        incidentTracker.addNewRecord(inputData[i]['id']);
+        incidentTracker.addNewRecordDate(inputData[i]['pubDate']);
       }
     }
   };
@@ -65,7 +101,6 @@ parseLatLong = function(latLngStrng){
   lng = parseFloat(splitLatLng[1]);
   splitLatLng[0] = lng;
   splitLatLng[1] = lat;
-  console.log(splitLatLng);
   return splitLatLng;
 };
 
@@ -76,6 +111,11 @@ download(url, function(data) {
       var targetData = result['rss']['channel'][0]['item'],
           incidentString = updateIncidents(targetData);
       writeToFile(incidentString);
+      console.log('Starting count: ' + incidentTracker.startingCount + '\n' + 
+                  'Records added: ' + + incidentTracker.objectsWritten  + '\n' + 
+                  'Ending count: ' + incidentTracker.endingCount() + '\n' + 
+                  'New Record IDs: ' + incidentTracker.newRecords  + '\n' +
+                  'New Record Dates: ' + incidentTracker.newRecordDates);
     });
   }
   else console.log("error");  
